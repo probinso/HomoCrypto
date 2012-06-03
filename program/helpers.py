f6e6e5723716496a77208cf705077f9e44b92d36
Kyle Brady <kyle@spark-gap.com>
2012-06-03 01:24:59 -0700

    Brought code closer to PEP8 standards

--- program/helpers.py
+++ program/helpers.py
@@ -2,13 +2,16 @@ from fractions import Fraction
 from math import ceil,log
 from random import randrange
 
+
 def intToBinList(i):
     # turns integer into binary list representation
     return map(int,bin(i)[2:])
 
+
 def binListToInt(l):
     # turns binary list into integer represenation
     return int(''.join(map(lambda x: str(int(x)),l)),2)
+
 
 def roundFrac(f):
     # rounds fraction to nearest integer
@@ -16,20 +19,24 @@ def roundFrac(f):
     if f - y >= Fraction(1,2): y +=1 
     return y
 
+
 def dotProduct(L1,L2):
     # returns dot product of two vectors
     assert(len(L1)==len(L2))
     return sum(map(lambda x: x[0]*x[1],zip(L1,L2)))
 
+
 def bitLims(B):
     # returns the bit limits for a number of bits
     return (2**(B-1),(2**B)-1)
+
 
 def randInt(B):
     # return int in bit range
     Min,Max = bitLims(abs(B))
     if B < 0: Min *= -1
     return randrange(Min,Max)
+
 
 def mods(x,n):
    # this is our symmetric modulou
@@ -38,23 +45,27 @@ def mods(x,n):
    if abs(a)<abs(b): return a
    else: return b
 
+
 def parityList(L):
     return map(lambda x : int(x%2), L)
 
+
 def intListToBinList(i):
-	#takes a string and returns a list of binary representation of each letter
-	newlist =[]
-	word = list(i)
-	for letter in word:
-		newlist.append(intToBinList(ord(letter)))
-	return newlist
+    #takes a string and returns a list of binary representation of each letter
+    newlist =[]
+    word = list(i)
+    for letter in word:
+        newlist.append(intToBinList(ord(letter)))
+    return newlist
+
 
 def wordBinListToString(l):
-	#takes a list of binary representations of letters and returns a string
-	word =""
-	for letter in l:
-		word += chr(binListToInt(letter))
-	return word
+    #takes a list of binary representations of letters and returns a string
+    word =""
+    for letter in l:
+        word += chr(binListToInt(letter))
+    return word
+
 
 def makeFixedWidthConverter(D):
     # takes in a fixed width and produces 
