d731921339e559c2660f603ca26ad313853b0013
Kyle Brady <kyle@spark-gap.com>
2012-06-03 01:58:51 -0700

    more cleanup

--- program/maybefhe.py
+++ program/maybefhe.py
@@ -1,4 +1,3 @@
-
 #Let's try this
 encS=asy.encryptSk(sk,pk,8)
 
@@ -6,10 +5,7 @@ encS=asy.encryptSk(sk,pk,8)
 cipher=asy.fheEncrypt([1,1,0,1],pk[0],pk[1],8)
 
 for bit in cipher:
-
     r=asy.dotProduct(encS,cipher[0][1])
     bit=(r,asy.multCipherHint(r,pk[1]))
-
-    
 
 asy.fheDecrypt(cipher,sk[1])
