331f8bcc5f1c16f4e11dd802a349d0f19b8805b2
Charlie <root@acm.wwu.edu>
2012-06-07 19:27:26 +0000

    added the html template for the website -Jeremy

--- program/views.py
+++ program/views.py
@@ -1,18 +1,19 @@
-from fractions import Fraction
+#from fractions import Fraction
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from HomoCrypto.program.helpers import *
 import HomoCrypto.program.asymhomo
-import os
+import HomoCrypto.program.client
+#import os
 
 with open('/home/homocrypto/rematch/homomorphic/HomoCrypto/program/gentry_pk.txt','r') as f:
 	pk = f.read().split(',')
 	pk = map(int,pk)
-	print len(pk)
+	#print len(pk)
         f.closed
 
 """
-with open('/home/homocrypto/rematch/homomorphic/HomoCrypto/program/gentry_pk.txt') as y:
+with open('/home/homocrypto/rematch/homomorphic/HomoCrypto/program/gentry_y.txt') as y:
 	yv = y.read().split(',')
 	yv = map(Fraction,yv)
 	print "and print that y vectage!"
@@ -23,29 +24,26 @@ def index(request):
 	return render_to_response('index.html', context_instance=RequestContext(request))
 
 def encrypt(request):
-	word = request.POST['search']
-        binlist = intListToBinList(word)
-	
+	try:
+		word = request.POST['search']
+		binlist = intListToBinList(word)
+	except:
+		return render_to_response('index.html',{'s': ["No initial input"]}, context_instance=RequestContext(request))
+
 	# sk,pk = HomoCrypto.program.asymhomo.fheKeyGen(8)
-
-	"""
-	with open('/home/homocrypto/rematch/homomorphic/HomoCrypto/program/gentry_pk.txt','r') as f:
-		pk = f.read().split(',')
-		pk = map(int,pk)
-	f.closed
 	
-	with open('/home/homocrypto/rematch/homomorphic/HomoCrypto/program/gentry_pk.txt') as y:
-		yv = y.read().split(',')
-		yv = map(Fraction,yv)
-	y.closed
-	"""
+	try:
+		cipher = HomoCrypto.program.asymhomo.encrypt(binlist, pk, 8)
+		cipher = map(lambda x: str(x)[:-1],cipher)
+	except:
+		cipher = ["Encrypt Error"]
 
-	cipher = HomoCrypto.program.asymhomo.encrypt(binlist, pk, 8)
-	cipher = map(lambda x: str(x)[:-1],cipher)
-
-	# os.system("140.160.137.13")
+	try:
+		HomoCrypto.program.client.sendciphertext(cipher)
+	except:
+		cipher = ["Hadoop listener is not running"] + cipher
 		
-	# print "and dat shit be encrypted!"
+	#dance party
 	
         return render_to_response('index.html',{'s': cipher}, context_instance=RequestContext(request))
 
