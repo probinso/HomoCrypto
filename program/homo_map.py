369574917dc0a57e8773db070813f01e8eac8608
hajya <alihaji484@gmail.com>
2012-06-07 00:08:21 -0700

    uhp

--- program/homo_map.py
+++ program/homo_map.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python
-
+from circuits import *
+from helpers import *
 import sys
 def main():
 	count=0
 
-	word="God"
+	word="god"
+	wordbin = intListToBinList(word)
 	start=True
-
 	for i in sys.stdin:
-	#if start:
-	#	word=i.strip()
-		line=i.strip().split()
-		for j in line:
-			if word==j:
-				count+=1
-
+		line = map(int,i[:-1])		
+		count += binListToInt(parityList(search(wordbin,intListToBinList("god god god hi hi"),[0],[0])))
 	print count
 
 main()
