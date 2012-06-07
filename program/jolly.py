edf5708d55c6c44b8004507eb69d20af50061df1
Philip Moss Robinson <robinsp3@CF498-xx.cs.wwu.edu>
2012-06-07 03:08:44 -0700

    fixed jolly

--- program/jolly.py
+++ program/jolly.py
@@ -1,11 +1,13 @@
 from asymhomo import encrypt
+from helpers import intListToBinList
+import os
 
-with open('./gentry_pk.txt','r') as f:
-    pk = r.read().split(',')
+with open("./gentry_pk.txt","r") as f:
+    pk = f.read().split(",")
     pk = map(int,pk)
-    f.close()
+    f.closed
 
 def jolly(word):
-    binlist = intToBinList(word)
+    binlist = intListToBinList(word)
     cipher = encrypt(binlist,pk,8)
     return cipher
